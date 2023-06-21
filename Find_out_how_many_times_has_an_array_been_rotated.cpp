08eba4002cbad2ae44c3c60e9eb312dbc92ac7d1
Vaibhav <vaibhavchaubey786@gmail.com>
2023-06-22 01:53:09 +0530

    Topic - Binary Search

--- Find_out_how_many_times_has_an_array_been_rotated.cpp
+++ Find_out_how_many_times_has_an_array_been_rotated.cpp
@@ -1,12 +1,7 @@
-/* Leetcode  Submission    (153. Find Minimum in Rotated Sorted Array) */
+/* GFG  Submission    (Rotation) */
 
-/* Problem Statement: Suppose an array of length n sorted in ascending order is rotated between 1 and n times. 
-For example, the array nums = [0,1,2,4,5,6,7] might become:
-[4,5,6,7,0,1,2] if it was rotated 4 times.
-[0,1,2,4,5,6,7] if it was rotated 7 times.
-Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
-Given the sorted rotated array nums of unique elements, return the minimum element of this array.
-You must write an algorithm that runs in O(log n) time. */
+/* Problem Statement: Given an ascending sorted rotated array Arr of distinct integers of size N. The array is right rotated K times. 
+Find the value of K. */
 
 
 /* Solution 1: Brute Force   */
@@ -14,17 +9,18 @@ You must write an algorithm that runs in O(log n) time. */
 // Space Complexity: O(1)
 
 
-// class Solution {
-// public:
-//     int findMin(vector<int>& nums) {
-//         int n = nums.size();
-//         int minEle = INT_MAX;
-//         for(int i = 0; i < n; i++){
-//             minEle = min(minEle, nums[i]);
-//         }
+// class Solution{
+// public:	
+// 	int findKRotation(int arr[], int n) {
+// 	    for(int i = 0; i < n-1; i++){
+// 	        if(arr[i] > arr[i+1]){
+// 	            return i+1;
+// 	        }
+// 	    }
+	    
+// 	    return 0;
+// 	}
 
-//         return minEle;
-//     }
 // };
 
 
@@ -33,17 +29,22 @@ You must write an algorithm that runs in O(log n) time. */
 // Time Complexity: O(logN)
 // Space Complexity: O(1)
 
+/* Approach: Whenever we get min element we update min element and take index of that min element in ans. At last return ans. */
 
-// class Solution {
-// public:
-//     int findMin(vector<int>& nums) {
-//         int n = nums.size();
-//         int minEle = INT_MAX;
+
+// class Solution{
+// public:	
+// 	int findKRotation(int arr[], int n) {
+// 	    int minEle = INT_MAX;
 //         int low = 0, high = n-1;
+//         int ans = 0;
 
 //         while(low <= high){
-//             if(nums[low] <= nums[high]){
-//                 minEle = min(minEle, nums[low]);
+//             if(arr[low] <= arr[high]){
+//                 if(arr[low] < minEle){
+//                     minEle = arr[low];
+//                     ans = low;
+//                 }
 //                 break;
 //             }
 //             int mid = low + (high - low) / 2;
@@ -51,22 +52,29 @@ You must write an algorithm that runs in O(log n) time. */
 //             // STEP 1: identifying sorted half
 
 //             // left sorted
-//             if(nums[low] <= nums[mid]){
+//             if(arr[low] <= arr[mid]){
 //                 // STEP 2: take min from that sorted part
-//                 minEle = min(minEle, nums[low]);
+//                 if(arr[low] < minEle){
+//                     minEle = arr[low];
+//                     ans = low;
+//                 }
 //                 // STEP 3: then eliminate that part
 //                 low = mid + 1;
 
 //             }
 //             // right sorted
-//             else {                          // if(nums[mid] <= nums[high])
+//             else {                          // if(arr[mid] <= arr[high])
 //                 // STEP 2: take min from that sorted part
-//                 minEle = min(minEle, nums[mid]);
+//                 if(arr[mid] < minEle){
+//                     minEle = arr[mid];
+//                     ans = mid;
+//                 }
 //                 // STEP 3: then eliminate that part
 //                 high = mid - 1;
 //             }
 //         }
 
-//         return minEle;
-//     }
+//         return ans;
+// 	}
+
 // };
